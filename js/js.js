892da187b13f8ba758bb68e382c0be02c2f5feba
andrew gibbs <andrewgibbs@outlook.com>
2016-09-05 12:11:52 -0700

    Next step is to add an api call to the info window

--- js/js.js
+++ js/js.js
@@ -110,7 +110,7 @@
           // Check to make sure the infowindow is not already opened on this marker.
           if (infowindow.marker != marker) {
               infowindow.marker = marker;
-              infowindow.setContent( '<div>' + marker.title +" "+ getLat(marker.title)+" "+ getLng(marker.title)+'</div>');
+              infowindow.setContent( '<div>' + marker.title +' '+ getLat(marker.title)+' '+ getLng(marker.title)+'</div>');
               infowindow.open(map, marker);
               // Make sure the marker property is cleared if the infowindow is closed.
               infowindow.addListener('closeclick', function() {
