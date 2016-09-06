b3d8022e3d68acfe79b4083c82b594240792e171
andrew gibbs <andrewgibbs@outlook.com>
2016-09-05 20:15:41 -0700

    Yelp api funcitonality added

--- server.js
+++ server.js
@@ -6,34 +6,34 @@ var cookieParser = require('cookie-parser');
 var rp           = require('request-promise');
 var bodyParser = require('body-parser');
 var app = express();
-
+var Yelp = require('yelp');
+var result;
 app.use(express.static(__dirname));
 
 
-var urlTerm = "https://gist.githubusercontent.com/erichurst/7882666/raw/5bdc46db47d9515269ab12ed6fb2850377fd869e/US%2520Zip%2520Codes%2520from%25202013%2520Government%2520Data";
-var YOUR_API_KEY ="AIzaSyCbt1MzT95dG4Tg9C-wF8BPH48Lyio3gok&v=3&callback=initMap";
-var revLat ="https://maps.googleapis.com/maps/api/geocode/json?latlng=40.7180628,-73.9961237&key=" + YOUR_API_KEY;
-rp(revLat)
-    .then(function (htmlString) {
-        // Process html...
-        // var collectLoc = htmlString;
-        // var hotPoata = collectLoc.split('\n');
-        // // console.log( hotPoata);
-        // for(var l in hotPoata){
-        //   if(hotPoata[l].split(",")[0] == "90018"){
-        //     console.log(hotPoata[l])
-        //   }
-        // }
-        //console.log(hotPoata[hotPoata.length-2].split(",")[0]);
-        var address = JSON.parse(htmlString).results[0].address_components;
-        var zipcode = address[address.length -2].long_name;
-        console.log(zipcode);
-          // console.log( JSON.parse(htmlString).results[0].address_components.length - 1.long_name);
-    })
-    .catch(function (err) {
-        // Crawling failed...
-        console.log("F for failed");
-    });
+var yelp = new Yelp({
+  consumer_key: 'BXh2TvLI9TerhMYdSxHvCw',
+  consumer_secret: 'PzeU5a8V5NltIdCldIbxGBEKtmo',
+  token: 'o07xHbkMyKyz1TmDHk0MfWEaOr4ngj4U',
+  token_secret: 'WBDdjTOkZoEZZ_w7-w4b4yAlFdQ',
+});
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+yelp.search({ term: 'food', /*location: 'Montreal'*/ll:'37.788022,-122.399797' ,total:5})
+.then(function (data) {
+  if(data.businesses.length>= 5){
+      result = data.businesses.slice(0,5);
+  }
+  else{
+    result = data.businesses.slice();
+  }
+  /* Take this data and push into react module for when a marker is pressed and populated
+  */
+  console.log(result[0]);
+})
+.catch(function (err) {
+  console.error(err);
+});
 
 //start listening on port 3000
 app.listen(3000);
