8f2765bc1a4ab2bc540303f7523e150e932e3da8
andrew gibbs <andrewgibbs@outlook.com>
2016-10-13 23:04:57 -0700

    ul list and li items added need to add clcik

--- src/js/optionTrigger.js
+++ src/js/optionTrigger.js
@@ -2,15 +2,15 @@
 //From the dynamically generated knockout view list, when a item within the select section is changed
 //the map marker corresponding to that selection will be triggered.
 
-$('option').click(function() {
+$('li').click(function() {
     $(this).addClass('mapLink');
 })
 $('select').change(function() {
     $(this).find(':selected').addClass('selected')
-        .siblings('option').removeClass('selected');
+        .siblings('li').removeClass('selected');
     $('.selected').text();
-    for (var i in $('option')) {
-        if ($('option')[i].text == $('.selected').text()) {
+    for (var i in $('li')) {
+        if ($('li')[i].text == $('.selected').text()) {
             google.maps.event.trigger(markers[i], 'click');
             // console.log("success",i,$('.selected').text());
         }
